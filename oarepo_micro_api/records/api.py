cacb4a2064715861c910dfd32674edc152272e68
Miroslav Bauer <mirekys@gmail.com>
2020-05-13 17:58:25 +0200

    secure record api with owner based permissions

--- oarepo_micro_api/records/api.py
+++ oarepo_micro_api/records/api.py
@@ -11,8 +11,11 @@ from __future__ import absolute_import, print_function
 
 from invenio_records_files.api import Record as FilesRecord
 
+from oarepo_micro_api.records.constants import ACL_ALLOWED_SCHEMAS, ACL_PREFERRED_SCHEMA
+
 
 class Record(FilesRecord):
-    """Custom record."""
-
+    """ACL enabled Demo Record."""
     _schema = "records/record-v1.0.0.json"
+    ALLOWED_SCHEMAS = ACL_ALLOWED_SCHEMAS
+    PREFERRED_SCHEMA = ACL_PREFERRED_SCHEMA
