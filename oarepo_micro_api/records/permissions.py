225383214f2c223f9077d22f3138afd0b837dfbb
Miroslav Bauer <mirekys@gmail.com>
2020-06-08 17:53:01 +0200

    implement attachment upload/downloads/list with owner based perms

--- oarepo_micro_api/records/permissions.py
+++ oarepo_micro_api/records/permissions.py
@@ -10,6 +10,10 @@ from elasticsearch_dsl import Q
 from flask_login import current_user
 from flask_principal import UserNeed, RoleNeed
 from invenio_access import Permission, authenticated_user, any_user
+from invenio_files_rest.models import Bucket, ObjectVersion, MultipartObject
+from invenio_files_rest.permissions import permission_factory, BucketRead
+
+from oarepo_micro_api.records.util import record_from_bucket
 
 
 def _get_owners(record):
@@ -46,6 +50,42 @@ def detail_permission_factory(record=None):
     if len(owners) > 0:
         return Permission(UserNeed(int(owners[0])))
     return Permission(any_user)
+
+
+def files_permission_factory(obj, action=None):
+    """Permissions factory for buckets."""
+    _read_actions = [
+        'files-rest-bucket-read',
+        'files-rest-bucket-read-versions',
+        'files-rest-object-read',
+        'files-rest-object-read-version',
+        'files-rest-multipart-read',
+        'files-rest-bucket-listmultiparts'
+    ]
+
+    _modify_actions = [
+        'files-rest-bucket-update',
+        'files-rest-object-delete'
+        'files-rest-object-delete-version',
+        'files-rest-multipart-delete'
+    ]
+
+    actionNeed = permission_factory(obj, action)
+    record = None
+    if isinstance(obj, Bucket):
+        record = record_from_bucket(obj.id)
+    elif isinstance(obj, ObjectVersion):
+        pass
+    elif isinstance(obj, MultipartObject):
+        pass
+
+    for need in actionNeed.explicit_needs:
+        if need.value in _read_actions:
+            return detail_permission_factory(record)
+        elif need.value in _modify_actions:
+            return owner_permission_factory(record)
+
+    return admin_permission_factory(obj, action)
 
 
 def owner_permission_filter(owned_only=False):
