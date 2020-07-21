7f6a47d2807361de562d5bfe490736d24201856d
Miroslav Bauer <mirekys@gmail.com>
2020-07-21 16:59:44 +0200

    fix well-known tests

--- tests/api/test_api_wellknown.py
+++ tests/api/test_api_wellknown.py
@@ -8,23 +8,27 @@
 """Test record and files."""
 
 from flask import url_for
-from webtest import AppError
 
 
-def test_readiness_probes(app, wsgi):
-    url = url_for('oarepo-heartbeat.liveliness', _external=True)
-    assert url.startswith('http://localhost/api/')
+def test_readiness_probes(wsgi):
+    url = '/.well-known/heartbeat/liveliness'
 
     res = wsgi.get(url)
     assert res.status_code != 404
 
 
-def test_liveliness_probes(app, wsgi):
-    url = url_for('oarepo-heartbeat.readiness', _external=True)
-    assert url.startswith('http://localhost/api/')
+def test_liveliness_probes(wsgi):
+    url = '/.well-known/heartbeat/readiness'
 
     res = wsgi.get(url)
     assert res.status_code != 404
+
+
+def test_environ(wsgi):
+    url = '/.well-known/heartbeat/environ'
+
+    res = wsgi.get(url)
+    assert res.status_code == 200
 
 
 def test_generic_api(wsgi):
