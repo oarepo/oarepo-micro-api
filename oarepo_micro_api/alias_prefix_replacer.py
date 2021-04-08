57282aa440a11b1ec2ee9396c5aa54dcb541aa38
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-08 12:20:53 +0200

    Fixed docstrings

--- oarepo_micro_api/alias_prefix_replacer.py
+++ oarepo_micro_api/alias_prefix_replacer.py
@@ -5,13 +5,9 @@ from wrapt import ObjectProxy
 
 
 class IndicesProxy(ObjectProxy):
-    """
-    Proxy that helps create mapping with replaced {PREFIX} placeholder
-    """
+    """Proxy that helps create mapping with replaced {PREFIX} placeholder."""
     def create(self, index=None, body=None, **kwargs):
-        """
-        Replace {PREFIX} placeholder in alias section in mapping
-        """
+        """Replace {PREFIX} placeholder in alias section in mapping."""
         if 'aliases' in body:
             prefix = current_app.config.get('SEARCH_INDEX_PREFIX', '')
             body['aliases'] = {
@@ -21,15 +17,14 @@ class IndicesProxy(ObjectProxy):
 
 
 class ElasticsearchProxy(ObjectProxy):
-    """
-    Proxy that helps create mapping with replaced {PREFIX} placeholder
-    """
+    """Proxy that helps create mapping with replaced {PREFIX} placeholder."""
     def __init__(self, wrapped):
         super().__init__(wrapped)
         self.__wrapped_indices__ = None
 
     @property
     def indices(self):
+        """Property that return indices"""
         if not self.__wrapped_indices__:
             self.__wrapped_indices__ = IndicesProxy(self.__wrapped__.indices)
         return self.__wrapped_indices__
@@ -37,9 +32,7 @@ class ElasticsearchProxy(ObjectProxy):
 
 @app_loaded.connect
 def patch_invenio(sender, *args, app=None, **kwargs):
-    """
-    Insert custom search client into invenio-search
-    """
+    """Insert custom search client into invenio-search."""
     with app.app_context():
         client = current_search.client
         if not isinstance(client, ElasticsearchProxy):
