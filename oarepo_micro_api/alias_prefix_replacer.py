b9b2cd85314cb81e1e7840f2d96f68f2ff99d2cb
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-08 12:33:31 +0200

    Fixed docstrings

--- oarepo_micro_api/alias_prefix_replacer.py
+++ oarepo_micro_api/alias_prefix_replacer.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CESNET.
+#
+# OARepo Micro API is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""OArepo Micro API alias prefix replacer."""
 from flask import current_app
 from invenio_base.signals import app_loaded
 from invenio_search import current_search
@@ -6,6 +14,7 @@ from wrapt import ObjectProxy
 
 class IndicesProxy(ObjectProxy):
     """Proxy that helps create mapping with replaced {PREFIX} placeholder."""
+
     def create(self, index=None, body=None, **kwargs):
         """Replace {PREFIX} placeholder in alias section in mapping."""
         if 'aliases' in body:
@@ -18,13 +27,15 @@ class IndicesProxy(ObjectProxy):
 
 class ElasticsearchProxy(ObjectProxy):
     """Proxy that helps create mapping with replaced {PREFIX} placeholder."""
+
     def __init__(self, wrapped):
+        """Init method."""
         super().__init__(wrapped)
         self.__wrapped_indices__ = None
 
     @property
     def indices(self):
-        """Property that return indices"""
+        """Property that return indices."""
         if not self.__wrapped_indices__:
             self.__wrapped_indices__ = IndicesProxy(self.__wrapped__.indices)
         return self.__wrapped_indices__
