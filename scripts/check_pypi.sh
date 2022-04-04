e098a98fea7df2df64bda0dcb33b41d8c5a6f96f
Tom Hlava <hlava@cesnet.cz>
2022-04-04 13:25:05 +0200

    check_pypi sleep increased

--- scripts/check_pypi.sh
+++ scripts/check_pypi.sh
@@ -11,7 +11,7 @@ set -e
 SETUP_PY='./setup.py'
 
 MAXI=4
-SLEEP=10
+SLEEP=20
 PKG="oarepo"
 OAREPO_VER=$(sed -n "/^OAREPO_VERSION/ { s/^OAREPO_VERSION = os.environ.get('OAREPO_VERSION', '\([0-9\.a-z]\+\)')/\1/; p; }" "${SETUP_PY}")
 
